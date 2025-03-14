fd1070208040cdfea6c7e1c7bf166093121ad0d0
Janhvi Badwaik <badwaikjanhvi@gmail.com>
2025-03-14 12:18:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+ "use strict";//treat all js code as newer version
+// //alert(3+3)//we are using nodejs, not browser
+console.log(3+3)
+console.log("janhvi")
+let name="janhvi"
+let age=18
+let isLoggedIn=false
+let state;
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+console.log(typeof undefined);//o/p: undefined
+console.log(typeof null);// o/p: object
