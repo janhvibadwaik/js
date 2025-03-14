0ce24290c1de25d9848c11ce4c479066035498b2
Janhvi Badwaik <badwaikjanhvi@gmail.com>
2025-03-14 12:22:35 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=144553
+let accountEmail="janhvi@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2
+
+accountEmail="hc@hc.com"
+accountPassword="2121212121"
+accountCity="Bengaluru"
+console.log(accountId);
+/* 
+prefer not to use var 
+because ofissue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
