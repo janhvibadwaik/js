c1b57f3de6c7f6e73860b5c6850626efa23dfd72
Janhvi Badwaik <badwaikjanhvi@gmail.com>
2025-03-15 06:55:27 +0000

    conversion of  datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,15 @@
+let score="33abc";// undefined-Nan;true-1;null-0;"janhvi"-Nan;
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let isLoggedIn=1;//""-false;"janhvi"-true
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+ let someNumber=33
+ let stringNumber=String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
